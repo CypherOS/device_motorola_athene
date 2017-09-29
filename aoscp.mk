70ca0bab8975e1dee8719594192d7cbfd8e69e86
rahulsnair <rahulsnair30@gmail.com>
2017-09-29 17:01:00 +0000

    athene : AOSCP Oreo bringup
    Change-Id: Ia9c0cf7dba2c6febc62363421b47012328284a8a

--- aoscp.mk
+++ aoscp.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
 
 $(call inherit-product, device/motorola/athene/full_athene.mk)
 
@@ -25,7 +25,7 @@ TARGET_BOOTANIMATION_HALF_RES := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := athene
-PRODUCT_NAME := lineage_athene
+PRODUCT_NAME := aoscp_athene
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := athene
